3d571875246f715939481df28be2bfc42ab87069
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-07-10 21:46:45 -0300

    feat: create dropdown list component

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -9,6 +9,6 @@ const Banner = () => {
       />
     </header>
   );
-}
+};
 
 export default Banner;
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,7 +1,18 @@
 import "./Form.css";
 import TextInput from "../TextInput";
+import DropdownList from "../DropdownList";
 
 const Form = () => {
+  const teams = [
+    "Programação",
+    "Front-end",
+    "Data Science",
+    "DevOps",
+    "Ux & Design",
+    "Mobile",
+    "Inovação e Gestão",
+  ];
+
   return (
     <section className="form">
       <form>
@@ -9,6 +20,7 @@ const Form = () => {
         <TextInput label="Nome" placeholder="Digite o seu nome" />
         <TextInput label="Cargo" placeholder="Digite o seu cargo" />
         <TextInput label="Imagem" placeholder="Digite o endereço de imagem" />
+        <DropdownList label="Times" items={teams} />
       </form>
     </section>
   );
