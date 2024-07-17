d64e9805dfd21c4773d508110ca27a6bd409c667
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-07-16 23:51:15 -0300

    feat: controlling component states

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,6 +2,7 @@ import "./Form.css";
 import TextInput from "../TextInput";
 import DropdownList from "../DropdownList";
 import Button from "../Button";
+import { useState } from "react";
 
 const Form = () => {
   const teams = [
@@ -14,6 +15,11 @@ const Form = () => {
     "Inovação e Gestão",
   ];
 
+  const [name, setName] = useState("");
+  const [position, setPosition] = useState("");
+  const [image, setImage] = useState("");
+  const [team, setTeam] = useState();
+
   const handleSubmit = (event) => {
     event.preventDefault();
   };
@@ -22,10 +28,33 @@ const Form = () => {
     <section className="form">
       <form onSubmit={handleSubmit}>
         <h2>Preencha os dados dos times de eSports da organização:</h2>
-        <TextInput required={true} label="Nome" placeholder="Digite o seu nome" />
-        <TextInput required={true} label="Cargo" placeholder="Digite o seu cargo" />
-        <TextInput label="Imagem" placeholder="Digite o endereço de imagem" />
-        <DropdownList required={true} label="Times" items={teams} />
+        <TextInput
+          required={true}
+          label="Nome"
+          placeholder="Digite o seu nome"
+          value={name}
+          onChange={(value) => setName(value)}
+        />
+        <TextInput
+          required={true}
+          label="Cargo"
+          placeholder="Digite o seu cargo"
+          value={position}
+          onChange={(value) => setPosition(value)}
+        />
+        <TextInput
+          label="Imagem"
+          placeholder="Digite o endereço de imagem"
+          value={image}
+          onChange={(value) => setImage(value)}
+        />
+        <DropdownList
+          required={true}
+          label="Times"
+          items={teams}
+          value={team}
+          onChange={(value) => setTeam(value)}
+        />
         <Button>Criar Card</Button>
       </form>
     </section>
--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -1,10 +1,19 @@
 import "./TextInput.css";
 
 const TextInput = (props) => {
+  const handleTyping = (event) => {
+    props.onChange(event.target.value);
+  };
+
   return (
     <div className="text-input">
       <label>{props.label}</label>
-      <input required={props.required} placeholder={props.placeholder} />
+      <input
+        value={props.inputValue}
+        onChange={handleTyping}
+        required={props.required}
+        placeholder={props.placeholder}
+      />
     </div>
   );
 };
